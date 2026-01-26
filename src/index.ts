d763e70332fbdd66c5bd68574014ff1c2b32acbc
phasenull <55253035+phasenull@users.noreply.github.com>
2026-01-26 18:13:37 +0300

    added useMutation, also fixed some return type stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { useEffect, useMemo, useState } from "@rbxts/react"
+import { useMemo, useState } from "@rbxts/react"
 
 export function useQuery<T>(args: {
 	queryKey: string[] | string
-	queryFn: () => T
+	queryFn: () => Promise<T>
+	enabled?: boolean
 }) {
 	const query_key_joined = typeOf(args.queryKey) === "table"
 		? (args.queryKey as string[]).join("|")
@@ -20,15 +21,49 @@ export function useQuery<T>(args: {
 	async function fetchData() {
 		setState((prev) => ({ ...prev, isLoading: true,data:undefined,err:undefined }))
 		try {
-			const result = args.queryFn()
+			const result = await args.queryFn() as T
+			if (result === undefined) {
+				error("Data returned from queryFn cannot be undefined")
+			}
 			setState({ data: result, isLoading: false, err: undefined })
 		} catch (err) {
 			setState({ data: undefined, isLoading: false, err: err })
 		}
 	}
 	useMemo(() => {
-		fetchData()
-	}, [query_key_joined])
+		if (args.enabled !== false) {
+			fetchData()
+		}
+	}, [query_key_joined, args.enabled])
 
 	return {...state, refetch: fetchData}
+}
+
+export function useMutation<TArgs extends unknown[], TData>(args: {
+	mutationFn: (...args: TArgs) => Promise<TData>
+}) {
+	const [state, setState] = useState<{
+		data: TData | undefined
+		isLoading: boolean
+		err: unknown
+	}>({
+		data: undefined,
+		isLoading: false,
+		err: undefined,
+	})
+
+	async function mutate(...mutationArgs: TArgs) {
+		setState((prev) => ({ ...prev, isLoading: true,data:undefined,err:undefined }))
+		try {
+			const result = await args.mutationFn(...mutationArgs) as TData
+			if (result === undefined) {
+				error("Data returned from mutationFn cannot be undefined")
+			}
+			setState({ data: result, isLoading: false, err: undefined })
+		} catch (err) {
+			setState({ data: undefined, isLoading: false, err: err })
+		}
+	}
+
+	return {...state, mutate}
 }
